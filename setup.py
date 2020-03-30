2310048dac637ed0a6e0f55ffc09bc8518a9f4b7
Fergus McClean <fergus.mcclean@ncl.ac.uk>
2020-03-30 09:25:53 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='intense',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.github.com/nclwater/intense-qc',
+    author='Fergus McClean',
+    author_email='fergus.mcclean@newcastle.ac.uk',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
