d14177e2b01fe01759c1cf31ad36eb767a60900e
David Pritchard <davidp100@hotmail.co.uk>
2020-05-11 21:42:52 +0100

    added test on rulebase output (QC'd data file)

--- tests/test_rulebase.py
+++ tests/test_rulebase.py
@@ -1,3 +1,4 @@
+import os
 from intense import rulebase
 from unittest import TestCase
 
@@ -8,3 +9,13 @@ summary_path = 'tests/test_output/Rulebase_Summary.csv'
 class TestRuleBase(TestCase):
     def test_main(self):
         rulebase.find_and_apply_rulebase(root_folder, summary_path)
+        
+        # Compare output QC'd data file with benchmark
+        benchmark_file_path = os.path.join(root_folder, "DE_02483/QCd_Data/DE_02483.txt")
+        test_output_path = "tests/test_output/QCd_Data/DE_02483.txt"
+        with open(benchmark_file_path, "r") as benchmark_file:
+            benchmark_output = benchmark_file.readlines()
+        with open(test_output_path, "r") as test_file:
+            test_output = test_file.readlines()
+        assert test_output == benchmark_output
+        
