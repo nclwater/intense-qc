f189f4b386d73955dc41ed146cd055a797a6d605
Fergus McClean <fergus.mcclean@ncl.ac.uk>
2021-02-18 17:08:43 +0000

    Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='intense',
-    version='0.2.0',
+    version='0.3.0.dev1',
     description='',
     long_description='',
     url='https://www.github.com/nclwater/intense-qc',
